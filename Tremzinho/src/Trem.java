50c790eb202f87a75490aa8cb437fc46ad721050
maarceloBjr <111317005+maarceloBjr@users.noreply.github.com>
2022-08-22 23:17:39 -0300

    Update Trem

--- Tremzinho/src/Trem.java
+++ Tremzinho/src/Trem.java
@@ -1,65 +1,87 @@
 import java.util.ArrayList;
 
 public class Trem {
-	private int identificador;
-	private ArrayList<Vagao> vagoes;
-	private ArrayList<Locomotiva> locomotivas;
+    private int identificador;
+    private ArrayList<Vagao> vagoes;
+    private ArrayList<Locomotiva> locomotivas;
 
-	public Trem(int identificador){
-		this.identificador = identificador;
-		vagoes = new ArrayList<>();
-		locomotivas = new ArrayList<>();
-	}
+    public Trem(int identificador){
+        this.identificador = identificador;
+        vagoes = new ArrayList<>();
+        locomotivas = new ArrayList<>();
+    }
 
-	public int getIdentificador() {
-		// TO DO
-	}
+    public int getIdentificador() {
+        return this.identificador;
+    }
 
-	public int getQtdadeLocomotivas() {
-		// TO DO
-	}
+    public int getQtdadeLocomotivas() {
+        return locomotivas.size();
+    }
 
-	public Locomotiva getLocomotiva(int posicao) {
-		// TO DO
-	}
+    public Locomotiva getLocomotiva(int posicao) {
+        return locomotivas.get(posicao);
+    }
 
-	public int getQtdadeVagoes() {
-		// TO DO
-	}
+    public int getQtdadeVagoes() {
+        return vagoes.size();
+    }
 
-	public Vagao getVagao(int posicao) {
-		// TO DO
-	}
+    public Vagao getVagao(int posicao) {
+        return vagoes.get(posicao);
+    }
 
-	private int maxVagoesNoTrem() {
-		// TO DO
-	}
+    private int maxVagoesNoTrem() {
+        int totalVagoes = 0;
+        for(Locomotiva locomotiva : locomotivas){
+            totalVagoes += locomotiva.getQtdadeMaxVagoes();
+        }
 
-	private double pesoMaxNoTrem() {
-		// TO DO
-	}
+        // for(int i = 0; i < locomotivas.size(); i++){
+        //     totalVagoes += locomotivas.get(i).getQtdadeMaxVagoes();
+        // }
 
-	private double pesoAtualDoTrem() {
-		// TO DO
-	}
+        totalVagoes = (int)(totalVagoes * (0.1 * (locomotivas.size()-1)));
+        return totalVagoes;
+    }
 
-	public boolean engataLocomotiva(Locomotiva locomotiva) {
-		// TO DO
-	}
+    private double pesoMaxNoTrem() {
+		double pesoMaximo = 0;
+		int totalVagoes = maxVagoesNoTrem();
+		for(Vagao vagao : vagoes){
+            if(vagao.getCapacidadeCarga() > pesoMaximo){
+				pesoMaximo = vagao.getCapacidadeCarga();
+			}
+        }
+		pesoMaximo = pesoMaximo * totalVagoes;
+		return pesoMaximo;
+    }
 
-	public boolean engataVagao(Vagao vagao) {
-		// TO DO
-	}
+    private double pesoAtualDoTrem() {
+		double pesoAtual = 0;
+		for(Vagao vagao : vagoes){
+				pesoAtual += vagao.getCapacidadeCarga();
+        }
+		return pesoAtual;
+    }
 
-	public boolean desengataLocomotiva() {
-		// TO DO
-	}
+    public boolean engataLocomotiva(Locomotiva locomotiva) {
+        // TO DO
+    }
 
-	public boolean desengataVagao() {
-		// TO DO
-	}
+    public boolean engataVagao(Vagao vagao) {
+        // TO DO
+    }
 
-	public String toString(){
-		// TO DO
-	}
+    public boolean desengataLocomotiva() {
+        // TO DO
+    }
+
+    public boolean desengataVagao() {
+        // TO DO
+    }
+
+    public String toString(){
+        // TO DO
+    }
 }
